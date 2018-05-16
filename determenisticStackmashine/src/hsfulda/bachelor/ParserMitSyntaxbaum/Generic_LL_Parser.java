c65431f777fb8ddcf96e1b39e9e59eb2de1f636d
Marcus <dont@dont-pc>
2018-05-16 11:07:26 +0200

    test / adding stack

--- determenisticStackmashine/src/hsfulda/bachelor/ParserMitSyntaxbaum/Generic_LL_Parser.java
+++ determenisticStackmashine/src/hsfulda/bachelor/ParserMitSyntaxbaum/Generic_LL_Parser.java
@@ -1,6 +1,6 @@
 package hsfulda.bachelor.ParserMitSyntaxbaum;
 
-import java.util.ArrayList;
+import java.util.*;
 
 //import  I_FlexibleGenericParserWithTree;
 
@@ -11,7 +11,8 @@ public class Generic_LL_Parser {
 	public ArrayList <Character> inputAlphabet = new ArrayList <Character>();
 	public ArrayList <String> stackAlphabet = new ArrayList <String>();
 	public String initialStackSymbol = new String();
-	
+	public Deque<Integer> stack = new ArrayDeque<Integer>();
+
 	
 	
 }
